60c8f96be2fb67dbc86c341034f379b13186c63c
SarveshD7 <sarvesh.ssd7@gmail.com>
2026-02-22 18:46:08 +0530

    Variables and Data types

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,6 +1,13 @@
 package main
+
 import "fmt"
 
 func main(){
 	fmt.Println("Hello World!")
+	var intNum int 
+	fmt.Println(intNum)
+	myVar := "text"
+	fmt.Println(myVar)
+	var1, var2 := 1,2
+	fmt.Println(var1, var2)
 }
